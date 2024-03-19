a2e0d5066ca471b27c9702d172df9da5cb0a566b
Archangel2903 <archangel2903@gmail.com>
2024-03-19 05:35:23 +0200

    aviator page

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,10 @@ import 'intersection-observer';
 import $ from 'jquery';
 import 'jquery-ui'
 import 'jquery-ui/ui/effect'
+import 'jquery-ui/ui/widgets/tabs';
 import 'bootstrap';
 import 'popper.js';
-import Swiper from 'swiper/dist/js/swiper.min';
-import noUiSlider from 'nouislider';
+import intlTelInput from 'intl-tel-input';
 
 $(window).on('load', function () {
     let b = $('body');
@@ -22,83 +22,58 @@ $(window).on('load', function () {
 });
 
 $(function () {
-    // Swiper slider
-    if ($('.swiper-container').length) {
-        let slider;
-        let slide = document.querySelectorAll('.swiper-container .swiper-slide').length;
+    // intl-tel-input
+    if ($('#phone').length) {
+        let input = document.querySelector("#phone");
+        let iti = intlTelInput(input, {
+            separateDialCode: true,
+            showSelectedDialCode: true,
+            utilsScript: "../../node_modules/intl-tel-input/build/js/utils.js",
+            initialCountry: "in",
+        });
 
-        if (slide > 1) {
-            slider = new Swiper('.swiper-container', {
-                observer: true,
-                observeParents: true,
-                loop: true,
-                autoplay: true,
-                spaceBetween: 25,
-                slidesPerView: 1,
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev'
-                },
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true
-                },
-                /*scrollbar: {
-                    el: '.swiper-scrollbar',
-                },*/
-                dynamicBullets: true,
-            });
-        }
+        // Обработчик события изменения страны
+        iti.promise.then(function () {
+            $("#countryCode").val(iti.getSelectedCountryData().dialCode);
+        });
+
+        // Обработчик события изменения телефонного номера
+        input.addEventListener("change", function () {
+            $("#countryCode").val(iti.getSelectedCountryData().dialCode);
+        });
     }
 
-    // Range slide
-    if ($('input[type="range"]')) {
-        let sliderRange = document.querySelectorAll('.slider-range');
-        let sliderHandles = document.querySelectorAll('.slider-handles');
-
-        if (sliderRange.length) {
-            sliderRange.forEach(function (elem) {
-                let input = elem.childNodes[0];
-                let startValue = input.hasAttribute('value') ? Number(input.getAttribute('value')) : 1;
-                let minValue = input.hasAttribute('min') ? Number(input.getAttribute('min')) : 1;
-                let maxValue = input.hasAttribute('max') ? Number(input.getAttribute('max')) : 100;
-
-                input.remove();
-
-                noUiSlider.create(elem, {
-                    start: [startValue],
-                    step: 1,
-                    behavior: 'tap',
-                    connect: [true, false],
-                    range: {
-                        'min': [minValue],
-                        'max': [maxValue]
-                    }
-                });
-            });
-        }
-
-        if (sliderHandles.length) {
-            sliderHandles.forEach(function (elem) {
-                let input = elem.childNodes[0];
-                let minValue = input.hasAttribute('min') ? Number(input.getAttribute('min')) : 1;
-                let maxValue = input.hasAttribute('max') ? Number(input.getAttribute('max')) : 100;
-
-                input.remove();
-
-                noUiSlider.create(elem, {
-                    start: [minValue, maxValue/2],
-                    step: 1,
-                    behavior: 'tap-drag',
-                    connect: true,
-                    range: {
-                        'min': minValue,
-                        'max': maxValue
-                    }
-                });
-            });
-        }
+    // Tabs
+    if ($('#tabs').length) {
+        $('#tabs').tabs({
+            show: {
+                effect: 'fadeIn',
+                duration: 300,
+            },
+            hide: {
+                effect: 'fadeOut',
+                duration: 300,
+            },
+        });
     }
+
+    // Password switch
+    const passBtn = document.querySelector('.form-button-pass');
+    let inputs = document.querySelectorAll('input[type="password"]');
+    passBtn.addEventListener('click', function () {
+        if (inputs[0].type === 'password') {
+            inputs.forEach(function(input) {
+                input.type = 'text';
+            });
+            this.innerHTML = '<svg><use xlink:href="img/spritemap.svg#sprite-pass-hidden"></use></svg>';
+        }
+        else {
+            inputs.forEach(function(input) {
+                input.type = 'password';
+            });
+            this.innerHTML = '<svg><use xlink:href="img/spritemap.svg#sprite-pass-visible"></use></svg>';
+        }
+    });
 
     // Lazy load observer
     const imagesAll = document.querySelectorAll('img[data-src]');
